a8ddef5337678be9d002aa62b0b5e0a0981690f2
tomJedusorr <raphael.dahomay@gmail.com>
2025-07-08 11:02:40 +0200

    adding a branch

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: 'build',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/my-portfolio', // <-- CHANGE this to match your GitHub repo name
 };
 
 export default nextConfig;
